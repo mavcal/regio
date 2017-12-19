8093e7da6593c4e71cba5fdd56d73621ff0d4462
Michael Calagaz <michael.calagaz@gmail.com>
2017-12-19 23:31:54 +1100

    Initial commit

---
+++ app.js
@@ -0,0 +1,86 @@
+﻿'use strict';
+var debug = require('debug');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var config = require('cloud-env');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+
+app.get('/:reg', function (req, res) {
+
+    if (req.params.reg == "ping") {
+        console.log(reg);
+        res.end("ping");
+    }
+    else {
+        var ps = require('./plate-snitch');
+        var reg = ps.getInfo(req.params.reg, res, callback);
+    }
+});
+
+var callback = function (res, data) {
+    console.log(data);
+    res.end(JSON.stringify(data));
+};
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_dns_name = config.HOSTNAME;
+
+var server = app.listen(server_port, server_ip_address, function () {
+    console.log("Listening on " + server_dns_name + ", port " + server_port)
+});
---
+++ plate-snitch.js
@@ -0,0 +1,46 @@
+﻿var Horseman = require('node-horseman'),
+    cheerio = require('cheerio');
+
+module.exports = {
+    // Open form and submit enquire for `rego`
+    getInfo: function (rego, res, callback) {
+        var horseman = new Horseman();
+        horseman
+            .userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0')
+            .open('https://www.vicroads.vic.gov.au/registration/buy-sell-or-transfer-a-vehicle/buy-a-vehicle/check-vehicle-registration/vehicle-registration-enquiry')
+            .waitForSelector('.btn-holder input')
+            .type('#registration-number-ctrl input[type=text]', rego)
+            .click('.btn-holder input')
+            .waitForNextPage({ timeout: 20000 })
+            .waitForSelector('.ctrl-holder.ctrl-readonly')
+            .html()
+            .then(function (body) {
+                var pInfo = module.exports.processInfo(body, rego);
+                //console.log(pInfo);
+                callback(res, pInfo);
+                return horseman.close();
+            });
+    },
+    // Scrape the results for key info
+    processInfo: function processInfo(html, rego) {
+        var $ = cheerio.load(html);
+        var vehicle = $('label.label').filter(function () {
+            return $(this).text().trim() === 'Vehicle:';
+        }).next().text().trim();
+
+        var stolen = $('label.label').filter(function () {
+            return $(this).text().trim() === 'Stolen status:';
+        }).next().text().trim();
+
+        var registration = $('label.label').filter(function () {
+            return $(this).text().trim() === 'Registration status & expiry date:';
+        }).next().text().trim();
+
+        return {
+            rego,
+            vehicle,
+            stolen,
+            registration
+        };
+    }
+}
